c76535fadfed2740564dc8f8695674bd38457eac
Vishnu Reddy <vishnureddy4545@outlook.com>
2018-10-21 21:35:13 +0530

    Update topicModeling.R

--- topicModeling.R
+++ topicModeling.R
@@ -14,8 +14,8 @@ library(topicmodels)
 #get listing of .txt files in directory
 #data <- list.files(getwd(),pattern="*.txt")
 
-data<-read.csv("filename.csv",header=TRUE,stringsAsFactors=FALSE)
-data <- data$Comment
+data<-read.csv("Game_of_thrones.csv",header=TRUE,stringsAsFactors=FALSE)
+data <- data$Text
 
 #read files into a character vector
 #files <- lapply(data,readLines)
@@ -32,10 +32,10 @@ corpus <- tm_map(corpus, removeNumbers);
 # remove generic and custom stopwords
 stopword <- c(stopwords('english'), "best");
 corpus <- tm_map(corpus, removeWords, stopword)
-corpus <- tm_map(corpus, stemDocument);
+#corpus <- tm_map(corpus, stemDocument);
 
 dtm<- DocumentTermMatrix(corpus, control = list(minWordLength = 3));
-dtm <- removeSparseTerms(dtm, sparse=0.98)
+#dtm <- removeSparseTerms(dtm, sparse=0.98)
 
 # k = 5;
 # SEED = 1234;
@@ -45,15 +45,18 @@ dtm <- removeSparseTerms(dtm, sparse=0.98)
 # lda.terms <- terms(article.lda)
 # lda.terms
 
-lda <- LDA(dtm, k = 3, control = list(seed = 1234))
+lda <- LDA(dtm, k = 5, control = list(seed = 1234))
 
 chapter_topics <- tidy(lda, matrix = "beta")
 
 top_n(chapter_topics, 10)
 
+Topic <- topics(lda, 1)
+Terms <- terms(lda, 30)
+
 top_terms <- chapter_topics %>%
   group_by(topic) %>%
-  top_n(5, beta) %>%
+  top_n(10, beta) %>%
   ungroup() %>%
   arrange(topic, -beta)
 
